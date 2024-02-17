6d9daf1ab6b6d1bfd799331c9a724c274ed6ab43
ayoub <ayoubousbia02@gmail.com>
2024-02-17 17:06:58 +0100

    add start using CRUD operation

--- main.go
+++ main.go
@@ -9,5 +9,8 @@ func main() {
 	router.HandleFunc("/hello-go", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world"))
 	})
+	router.HandleFunc("POST /user", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Created User"))
+	})
 	http.ListenAndServe(":8000", router)
 }
